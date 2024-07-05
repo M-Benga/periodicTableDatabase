da9abcfdc2e0ebed3f5e312e1f781d612886f3d4
M-Benga Mohamedi <hamism77@gmail.com>
2024-07-05 06:14:10 +0000

    feat: add guiding comments

--- element.sh
+++ element.sh
@@ -3,6 +3,11 @@
 MAIN_MENU() {
 echo "Please provide an element as an argument."
 read INPUT
+ 
+# Check if INPUT exist in symbol column, else
+# Check if  INPUT exist in a names column 
+# Check if INPUT exist in an atomic numbers column
+# It doesn't exist anywhere in our database
 
 }
 
